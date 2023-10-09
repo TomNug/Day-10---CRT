8789e698ef6ec7788f117a2991a5d774c4772fed
t.nugent <t.nugent@hotmail.co.uk>
2023-10-09 13:31:19 +0100

    Began the rendering. Cycle values within 1 of register values means render the pixel.
    Made the Render method - appears to render the first line only, leaving the others as .

--- Day 10 - CRT/Program.cs
+++ Day 10 - CRT/Program.cs
@@ -1,4 +1,6 @@
-﻿class CPU
+﻿using System.Text; // For StringBuilder
+
+class CPU
 {
     private int rX; // register
     private int cycle;
@@ -7,6 +9,9 @@
     string[] instructions;
     int instructionPtr;
     int signalSum;
+    // Renderer
+    char[] renderView;
+
     public bool MoreInstructions()
     {
         return instructionPtr < instructions.Length;
@@ -20,6 +25,13 @@
         instructions = inputInstructions;
         instructionPtr = 0;
         signalSum = 0;
+        renderView = new char[240];
+        char fillCharacter = '.';
+        for (int i = 0; i < renderView.Length; i++)
+        {
+            renderView[i] = fillCharacter;
+        }
+
     }
     public int GetSignalStrength()
     {
@@ -68,9 +80,39 @@
             // Resolve current instruction
             rX += acc;
         }
+
+        // Determine if the pixel is within the currently rendered element
+        if (Math.Abs(cycle - rX) <= 1)
+        {
+            // Currect cycle is within 1 of the current register 
+            // This means render the pixel
+            // Problem is communicated from 1-240
+            // Execution is implemented in an array which must be 0-239
+            // Hence -1
+            renderView[cycle-1] = '#';
+        } else
+        {
+            renderView[cycle-1] = '.';
+        }
+
+
+
         cycle++;
     }
+    public void Render()
+    {
+        for (int row = 0; row < 6; row++)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int col = 0; col < 40; col++)
+            {
+                sb.Append(renderView[row * 40 + col]);
+            }
+            Console.WriteLine(sb.ToString());
+        }
 
+
+    }
     public void Compute()
     {
         while (MoreInstructions() || wait > 0)
@@ -100,15 +142,15 @@ class Program
     public static void Main(string[] args)
     {
         //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test.txt");
-        //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test_larger.txt");
-        string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_full.txt");
+        string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test_larger.txt");
+        //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_full.txt");
 
 
         CPU cpu = new CPU(instructions);
 
         
         cpu.Compute();
-
+        cpu.Render();
         Console.WriteLine(String.Format("Solution is {0}", cpu.GetSignalStrength()));
     }
 }
