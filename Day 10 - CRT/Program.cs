7f55bee8e46b4414a5ed257b6167bbc86d3bbb36
t.nugent <t.nugent@hotmail.co.uk>
2023-10-23 14:03:57 +0100

    2* solution. Renamed variables to match change of context in stage 2.

--- Day 10 - CRT/Program.cs
+++ Day 10 - CRT/Program.cs
@@ -2,15 +2,14 @@
 
 class CPU
 {
-    private int rX; // register
+    private int spritePos; 
     private int cycle;
     private int wait;
     private int acc;
-    string[] instructions;
-    int instructionPtr;
-    int signalSum;
+    private string[] instructions;
+    private int instructionPtr;
     // Renderer
-    char[] renderView;
+    private char[] renderView;
 
     public bool MoreInstructions()
     {
@@ -18,13 +17,12 @@ class CPU
     }
     public CPU(string[] inputInstructions)
     {
-        rX = 1;
+        spritePos = 1;
         cycle = 1;
         wait = 0;
         acc = 0;
         instructions = inputInstructions;
         instructionPtr = 0;
-        signalSum = 0;
         renderView = new char[240];
         char fillCharacter = '.';
         for (int i = 0; i < renderView.Length; i++)
@@ -32,10 +30,6 @@ class CPU
             renderView[i] = fillCharacter;
         }
 
-    }
-    public int GetSignalStrength()
-    {
-        return signalSum;
     }
     public void FetchAndDecode()
     {
@@ -69,6 +63,27 @@ class CPU
                 FetchAndDecode();
             }
         }
+
+        // Render occurs DURING the cycle
+        //Console.WriteLine(String.Format("Calculated sprite position as {0}\t - Cycle is {1}", rX, ((cycle - 1) % 40) + 1));
+        int pixel = ((cycle - 1) % 40);
+        if (Math.Abs(pixel - spritePos) <= 1)
+        {
+            // Currect pixel is within 1 of the sprite
+            // This means render the pixel
+            // Problem is communicated from 1-240
+            // Execution is implemented in an array which must be 0-239
+            // Hence -1
+            renderView[cycle - 1] = '#';
+        }
+        else
+        {
+            renderView[cycle - 1] = '.';
+        }
+
+        // *************
+        // END OF CYCLE
+
         // Decrement wait time
         if (wait > 0)
         {
@@ -78,22 +93,11 @@ class CPU
         if (wait == 0)
         {
             // Resolve current instruction
-            rX += acc;
+            spritePos += acc;
+            //Console.WriteLine("Register has changed to", spritePos);
         }
 
-        // Determine if the pixel is within the currently rendered element
-        if (Math.Abs(cycle - rX) <= 1)
-        {
-            // Currect cycle is within 1 of the current register 
-            // This means render the pixel
-            // Problem is communicated from 1-240
-            // Execution is implemented in an array which must be 0-239
-            // Hence -1
-            renderView[cycle-1] = '#';
-        } else
-        {
-            renderView[cycle-1] = '.';
-        }
+
 
 
 
@@ -122,16 +126,11 @@ class CPU
 
             // Start
             //Console.WriteLine(String.Format("Start of cycle {0} \tX is {1}\t Acc is {2}\t Wait is {3}", cycle, rX, acc, wait));
-            
-            // Report the signal strengths at the given times
-            if (cycle == 20 || (cycle-20) % 40 == 0)
-            {
-                Console.WriteLine(String.Format("### {0}th cycle\tSignal Strength: {1}", cycle, cycle * rX));
-                signalSum += cycle * rX;
-            }
 
+            // Step the processor forward one cycle
             Tick();
-            //Console.WriteLine(String.Format("After of cycle {0} \tX is {1}\t Acc is {2}\t Wait is {3}", cycle-1, rX, acc, wait));
+
+            //Console.WriteLine(String.Format("After cycle {0} \tX is {1}\t Acc is {2}\t Wait is {3}\n======================\n\n", cycle-1, rX, acc, wait));
 
         }
     }
@@ -142,15 +141,17 @@ class Program
     public static void Main(string[] args)
     {
         //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test.txt");
-        string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test_larger.txt");
-        //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_full.txt");
+        //string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_test_larger.txt");
+        string[] instructions = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day 10 - CRT\Day 10 - CRT\data_full.txt");
 
 
         CPU cpu = new CPU(instructions);
 
-        
+        // Start the processor
+        // Read and execute instructions until none remain 
         cpu.Compute();
+        
+        // Output the grid of pixels
         cpu.Render();
-        Console.WriteLine(String.Format("Solution is {0}", cpu.GetSignalStrength()));
     }
 }
